96df721168c509516a995eb4de0e2603ab340d1f
Manoj Lonkar <manojlonkar1533@gmail.com>
2023-03-16 13:09:00 +0530

    added steup and requirement file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+def get_requirements(fiel_path:str)->list[str]:
+    '''
+    This function will return required file
+
+    '''
+    requirements=[]
+    with open(fiel_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+
+
+setup(
+name = 'mlproject',
+version='0.0.1',
+author='Manoj',
+packages=find_packages(),
+install_requires = get_requirements('requirements.txt')
+)
